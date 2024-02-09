259cd55fa9740216d8985fe1637dde5a4a440e27
devMiiaHxp <dopemiiahxy@gmail.com>
2024-02-09 07:15:04 +0100

    PrizaX

---
+++ src/partials/FeaturesBlocks.jsx
@@ -0,0 +1,318 @@
+import React from "react";
+
+function FeaturesBlocks() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4">Why PrizaX?</h2>
+            <p className="text-xl text-gray-400">
+              &nbsp;
+              <li>
+                Our courses seamlessly integrate online and on-site training
+                offering a flexible approach to meet the unique needs of
+                security-focused professionals.
+              </li>
+              &nbsp;
+              <li>
+                Our seasoned instructors are devoted to imparting skills that
+                lead to tangible outcomes, enhancing both professional and
+                personal efficiency.
+              </li>
+              &nbsp;
+              <li>
+                PrizaX's diverse courses not only elevate workforce capabilities
+                but also empower citizens, contributing to a digitally skilled
+                and efficient society.
+              </li>
+            </p>
+          </div>
+
+          {/* Items */}
+          <div
+            className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none"
+            data-aos-id-blocks
+          >
+            {/* 1st item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              {/* <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
+              </svg> */}
+              <svg
+                className="w-16 h-16 mb-4 bg-purple-600 rounded-full p-3"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                 
+                <path 
+                className="stroke-current text-purple-100 "
+                d="m17.994 2.286a9 9 0 0 0 -14.919 5.536 8.938 8.938 0 0 0 2.793 7.761 6.263 6.263 0 0 1 2.132 4.566v.161a3.694 3.694 0 0 0 3.69 3.69h.62a3.694 3.694 0 0 0 3.69-3.69v-.549a5.323 5.323 0 0 1 1.932-4 8.994 8.994 0 0 0 .062-13.477zm-5.684 19.714h-.62a1.692 1.692 0 0 1 -1.69-1.69s-.007-.26-.008-.31h4.008v.31a1.692 1.692 0 0 1 -1.69 1.69zm4.3-7.741a7.667 7.667 0 0 0 -2.364 3.741h-1.246v-7.184a3 3 0 0 0 2-2.816 1 1 0 0 0 -2 0 1 1 0 0 1 -2 0 1 1 0 0 0 -2 0 3 3 0 0 0 2 2.816v7.184h-1.322a8.634 8.634 0 0 0 -2.448-3.881 7 7 0 0 1 3.951-12.073 7.452 7.452 0 0 1 .828-.046 6.921 6.921 0 0 1 4.652 1.778 6.993 6.993 0 0 1 -.048 10.481z"
+                strokeLinecap="round" strokeWidth="1" fill="none" fillRule="evenodd"
+                />
+              </svg>
+              {/* <svg
+                className="w-16 h-16 mb-4 bg-purple-600 rounded-full p-2"
+                id="Layer_1"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                data-name="Layer 1"
+              >
+                <path
+                  className="stroke-current text-purple-100"
+                  d="m17.994 2.286a9 9 0 0 0 -14.919 5.536 8.938 8.938 0 0 0 2.793 7.761 6.263 6.263 0 0 1 2.132 4.566v.161a3.694 3.694 0 0 0 3.69 3.69h.62a3.694 3.694 0 0 0 3.69-3.69v-.549a5.323 5.323 0 0 1 1.932-4 8.994 8.994 0 0 0 .062-13.477zm-5.684 19.714h-.62a1.692 1.692 0 0 1 -1.69-1.69s-.007-.26-.008-.31h4.008v.31a1.692 1.692 0 0 1 -1.69 1.69zm4.3-7.741a7.667 7.667 0 0 0 -2.364 3.741h-1.246v-7.184a3 3 0 0 0 2-2.816 1 1 0 0 0 -2 0 1 1 0 0 1 -2 0 1 1 0 0 0 -2 0 3 3 0 0 0 2 2.816v7.184h-1.322a8.634 8.634 0 0 0 -2.448-3.881 7 7 0 0 1 3.951-12.073 7.452 7.452 0 0 1 .828-.046 6.921 6.921 0 0 1 4.652 1.778 6.993 6.993 0 0 1 -.048 10.481z"
+                />
+              </svg> */}
+              <h4 className="h4 mb-2">Instant Features</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat.
+              </p>
+            </div>
+
+            {/* 2nd item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="100"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              {/* <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  className="fill-current text-purple-600"
+                  cx="32"
+                  cy="32"
+                  r="32"
+                />
+                <path
+                  className="stroke-current text-purple-100"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                  d="M21 23h22v18H21z"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+                <path
+                  className="stroke-current text-purple-300"
+                  d="M26 28h12M26 32h12M26 36h5"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                />
+              </svg> */}
+              <svg className="w-16 h-16 mb-4 bg-purple-600 rounded-full p-3" xmlns="http://www.w3.org/2000/svg" id="Filled" viewBox="0 0 24 24" width="512" height="512"><path className="stroke-current text-purple-100 " fill="none" d="M1.327,12.4,4.887,15,3.535,19.187A3.178,3.178,0,0,0,4.719,22.8a3.177,3.177,0,0,0,3.8-.019L12,20.219l3.482,2.559a3.227,3.227,0,0,0,4.983-3.591L19.113,15l3.56-2.6a3.227,3.227,0,0,0-1.9-5.832H16.4L15.073,2.432a3.227,3.227,0,0,0-6.146,0L7.6,6.568H3.231a3.227,3.227,0,0,0-1.9,5.832Z"/></svg>
+
+              <h4 className="h4 mb-2">Instant Features</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat.
+              </p>
+            </div>
+
+            {/* 3rd item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="200"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-purple-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <g
+                  transform="translate(21 21)"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <ellipse
+                    className="stroke-current text-purple-300"
+                    cx="11"
+                    cy="11"
+                    rx="5.5"
+                    ry="11"
+                  />
+                  <path
+                    className="stroke-current text-purple-100"
+                    d="M11 0v22M0 11h22"
+                  />
+                  <circle
+                    className="stroke-current text-purple-100"
+                    cx="11"
+                    cy="11"
+                    r="11"
+                  />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Instant Features</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat.
+              </p>
+            </div>
+
+            {/* 4th item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="300"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-purple-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <g
+                  transform="translate(22 21)"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <path
+                    className="stroke-current text-purple-100"
+                    d="M17 22v-6.3a8.97 8.97 0 003-6.569A9.1 9.1 0 0011.262 0 9 9 0 002 9v1l-2 5 2 1v4a2 2 0 002 2h4a5 5 0 005-5v-5"
+                  />
+                  <circle
+                    className="stroke-current text-purple-300"
+                    cx="13"
+                    cy="9"
+                    r="3"
+                  />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Instant Features</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat.
+              </p>
+            </div>
+
+            {/* 5th item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="400"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-purple-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <g
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <path
+                    className="stroke-current text-purple-100"
+                    d="M29 42h10.229a2 2 0 001.912-1.412l2.769-9A2 2 0 0042 29h-7v-4c0-2.373-1.251-3.494-2.764-3.86a1.006 1.006 0 00-1.236.979V26l-5 6"
+                  />
+                  <path
+                    className="stroke-current text-purple-300"
+                    d="M22 30h4v12h-4z"
+                  />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Instant Features</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat.
+              </p>
+            </div>
+
+            {/* 6th item */}
+            <div
+              className="relative flex flex-col items-center"
+              data-aos="fade-up"
+              data-aos-delay="500"
+              data-aos-anchor="[data-aos-id-blocks]"
+            >
+              <svg
+                className="w-16 h-16 mb-4"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect
+                  className="fill-current text-purple-600"
+                  width="64"
+                  height="64"
+                  rx="32"
+                />
+                <g
+                  transform="translate(21 22)"
+                  strokeLinecap="square"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                >
+                  <path
+                    className="stroke-current text-purple-300"
+                    d="M17 2V0M19.121 2.879l1.415-1.415M20 5h2M19.121 7.121l1.415 1.415M17 8v2M14.879 7.121l-1.415 1.415M14 5h-2M14.879 2.879l-1.415-1.415"
+                  />
+                  <circle
+                    className="stroke-current text-purple-300"
+                    cx="17"
+                    cy="5"
+                    r="3"
+                  />
+                  <path
+                    className="stroke-current text-purple-100"
+                    d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948"
+                  />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2">Instant Features</h4>
+              <p className="text-lg text-gray-400 text-center">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FeaturesBlocks;
